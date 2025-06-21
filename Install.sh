5643e1f89f3bc17b8058c90710b1dac0646de5f3
OpyrusDevOps <opyrusdeveloper@gmail.com>
2025-06-21 23:09:31 +0200

    Update: Install.sh

--- Install.sh
+++ Install.sh
@@ -1,5 +1,22 @@
-echo "Installing git ..."
-sudo pacman -S git
+Installation() {
+  Installing_Message $1
+  sudo pacman -S $2
+}
+
+Install_Dotnet() {
+  Installing_Message Dotnet
+  curl -L https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
+  chmod +rx dotnet-install.sh
+  ./dotnet-install.sh --version latest
+}
+
+Installing_Message() {
+  echo "Installing $1 ..."
+}
+
+echo "####  App Installion  ####"
+
+Installation "git" git
 
 echo "Install Neovim Config ..."
 git clone https://github.com/OpyrusDevOp/Neovim-Config.git ~/.config/nvim
@@ -7,35 +24,45 @@ git clone https://github.com/OpyrusDevOp/Neovim-Config.git ~/.config/nvim
 echo "Installing Yay ..."
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
-echo "Install Terminal ..."
-sudo pacman -S kitty
+Installation "Terminal" kitty
 
-echo "Install Shell ..."
-sudo pacman -S fish
+Installation "Neovim" neovim
 
-echo "Install Browser ..."
-sudo pacman -S firefox
+Installation "Shell" fish
 
-echo "Install AppLauncher ..."
-sudo pacman -S fuzzel
+Installation "Browser" firefox
 
-echo "Install Dock ..."
-sudo pacman -S waybar
+Installation "AppLauncher" fuzzel
 
-echo "Install Wallpaper daemon ..."
-sudo pacman -S hyprpaper
+Installation "Dock" waybar
 
-echo "Install File Manager ..."
-sudo pacman -S nautilus
+Installation "Wallpaper daemon" hyprpaper
 
-echo "Install Terminal Session Manager ..."
-sudo pacman -S zellij
+Installation "File Manager" nautilus
 
-echo "Install Discord ..."
-sudo pacman -S discord
+Installation "Terminal Session Manager" zellij
 
-echo "Install Obsidian ..."
-sudo pacman -S obsidian
+Installation "Discord" discord
 
-echo "Install Bun ..."
+Installation "Obsidian" obsidian
+
+Installation "Libre Office" libreoffice
+
+Installation "Unzip" unzip
+
+Installation "Keychain" keychain
+
+Installation "Brightness Control" brightnessctl
+
+Install_Dotnet
+
+Installing_Message Bun
 curl -fsSL https://bun.sh/install | bash
+
+Installing_Message "Nvm"
+yay -S nvm
+
+echo "#### Configuration ####"
+mkdir -p ~/{Documents, Musics, Pictures, Projects/{Softwares, Games}, Videos}
+mkdir -p /usr/local/share/fonts/{otf,ttf}
+unzip ./JetBrainsMono.zip -d /usr/local/share/fonts/ttf/
--- README.md
+++ README.md
@@ -7,16 +7,20 @@
 
 ## Apps
 
-- **Shell** : fish
+- **Shell** : *fish*
 - **Web browser** : *Firefox*
 - **App Launcher** : *Fuzzel*
 - **Dock** : *Waybar*
 - **Wallpaper** : *Hyprpaper*
 - **File manager** : *Nautilus*
 - **Terminal** : *Kitty*
+- **Editor** : *Neovim*
 - **Terminal Session** : *Zellij*
 - **Music Player** : [*Harmonia*](https://github.com/OpyrusDevOp/Harmonia.git)
+- **SSH session key Manager** : *Keychain*
 - **AUR** : *Yay*
+- **JS/TS** : *Bun, Npm*
+- **Office suite** : *Libre Office*
 - *Git*
 - *Discord*
 - *Obsidian*
